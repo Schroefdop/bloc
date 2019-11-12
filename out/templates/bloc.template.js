78d8f24f2e136a9ae932cf9cb69cf530da393007
Wouter Vermeij <wouter.vermeij@elements.nl>
2019-11-12 10:32:36 +0100

    renew the package

--- out/templates/bloc.template.js
+++ out/templates/bloc.template.js
@@ -3,13 +3,16 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const changeCase = require("change-case");
 function getBlocTemplate(blocName) {
     const pascalCaseBlocName = changeCase.pascalCase(blocName.toLowerCase());
+    const blocType = `${pascalCaseBlocName}BlocType`;
     const blocState = `${pascalCaseBlocName}State`;
     const blocEvent = `${pascalCaseBlocName}Event`;
     return `import 'dart:async';
 import 'package:bloc/bloc.dart';
 import './bloc.dart';
 
-class ${pascalCaseBlocName}Bloc extends Bloc<${blocEvent}, ${blocState}> {
+abstract class ${blocType} extends Bloc<${blocEvent}, ${blocState}> {}
+
+class ${pascalCaseBlocName}Bloc extends ${blocType} {
   @override
   ${blocState} get initialState => ${blocState}.initial();
 
